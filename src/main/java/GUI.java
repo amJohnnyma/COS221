9a456420303b50692155d8e245c948517beb5160
amJohnnyma <dewaldcoleskydc@gmail.com>
2025-04-17 10:21:40 +0200

    maven project

---
+++ src/main/java/GUI.java
@@ -0,0 +1,22 @@
+
+import java.io.*;
+import javax.swing.*;
+
+class GUI
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Click");
+
+        button.setBound(15,15,200,50);
+
+        frame.add(button);
+
+        frame.setSize(500,600);
+
+        frame.setLayout(null);
+
+        frame.setVisible(true);
+    }
+}
